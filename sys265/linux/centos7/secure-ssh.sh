9845f55a7f63ff83f45a9a344503b379b07672cd
natalie363 <natalie.eckles@mymail.champlain.edu>
2023-02-15 14:00:39 +0000

    More SSH Script Edits

--- sys265/linux/centos7/secure-ssh.sh
+++ sys265/linux/centos7/secure-ssh.sh
@@ -5,15 +5,15 @@
 sudo useradd -m -d /home/${1} -s /bin/bash ${1}
 sudo mkdir /home/${1}/.ssh
 # adds a public key from the local repo or curled from the remote repo
-sudo cd ~/tech-journals
-sudo cp SYS265/linux/public-keys/id_rsa.pub /home/${1}/.ssh/authorized_keys
-sudo chmod 700 /home/${1}.ssh
+cd /home/natalie-adm/tech-journals/
+sudo cp sys265/linux/public-keys/id_rsa.pub /home/${1}/.ssh/authorized_keys
+sudo chmod 700 /home/${1}/.ssh
 sudo chmod 600 /home/${1}/.ssh/authorized_keys
 sudo chown -R ${1}:${1} /home/${1}/.ssh
 # removes roots ability to ssh in
-cat /etc/issue >> $version
-if [["$version" == "Ubuntu 20.04.5 LTS \n \l"]]
-then  
+version=$( cat /etc/issue )
+ubuntu="Ubuntu 20.04.5 LTS"
+if [[ "$version" == "$ubuntu" ]]; then  
   echo "This is Ubuntu"
 else
   cd /etc/ssh
