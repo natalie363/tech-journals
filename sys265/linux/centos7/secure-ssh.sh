ad33d9fbc7129a9775f1410eec2522a1993f04af
natalie363 <natalie.eckles@mymail.champlain.edu>
2023-02-15 01:05:31 +0000

    SSH Script Update

--- sys265/linux/centos7/secure-ssh.sh
+++ sys265/linux/centos7/secure-ssh.sh
@@ -1,7 +1,14 @@
 # secure-ssh.sh
 # author natalie363
 # creates a new ssh user using $1 parameter
+useradd -m -d /home/$1 -g /bin/bash $1
+mkdir /home/$1/.ssh
 # adds a public key from the local repo or curled from the remote repo
+cd ~/tech-journals
+sudo cp SYS265/linux/public-keys/id_rsa.pub /home/$1/.ssh/authorized_keys
+sudo chmod 700 /home/$1.ssh
+sudo chmod 600 /home/$1/.ssh/authorized_keys
+sudo chown -R $1:$1 /home/$1/.ssh
 # removes roots ability to ssh in
 cat /etc/issue >> $version
 if [["$version" == "Ubuntu 20.04.5 LTS \n \l"]]
