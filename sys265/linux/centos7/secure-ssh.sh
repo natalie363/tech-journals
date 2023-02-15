db0d567086f376675fab5e671f6fb532c0cab014
natalie363 <natalie.eckles@mymail.champlain.edu>
2023-02-15 01:32:38 +0000

    SSH Script Update

--- sys265/linux/centos7/secure-ssh.sh
+++ sys265/linux/centos7/secure-ssh.sh
@@ -2,10 +2,10 @@
 # secure-ssh.sh
 # author natalie363
 # creates a new ssh user using $1 parameter
-useradd -m -d /home/${1} -g /bin/bash ${1}
-mkdir /home/${1}/.ssh
+sudo useradd -m -d /home/${1} -s /bin/bash ${1}
+sudo mkdir /home/${1}/.ssh
 # adds a public key from the local repo or curled from the remote repo
-cd ~/tech-journals
+sudo cd ~/tech-journals
 sudo cp SYS265/linux/public-keys/id_rsa.pub /home/${1}/.ssh/authorized_keys
 sudo chmod 700 /home/${1}.ssh
 sudo chmod 600 /home/${1}/.ssh/authorized_keys
