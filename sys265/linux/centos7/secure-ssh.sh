47626da28e725637ac48d38c73bf00910d29594e
natalie363 <natalie.eckles@mymail.champlain.edu>
2023-02-17 19:04:19 +0000

    SSH Script Changes

--- sys265/linux/centos7/secure-ssh.sh
+++ sys265/linux/centos7/secure-ssh.sh
@@ -13,8 +13,10 @@ sudo chown -R ${1}:${1} /home/${1}/.ssh
 # removes roots ability to ssh in
 version=$( cat /etc/issue )
 ubuntu="Ubuntu 20.04.5 LTS"
-if [[ "$version" == "$ubuntu" ]]; then  
-  echo "This is Ubuntu"
+if [[ "$version" == "$ubuntu" ]]; then
+  cd /etc/ssh
+  sed -i "s/PermitRootLogin/#PermitRootLogin/I" sshd_config
+  systemctl restart sshd.service  
 else
   cd /etc/ssh
   sed -i "s/#PermitRootLogin yes/PermitRootLogin no/I" sshd_config
